597523ff7ca73edeb1a0f3837aca86b827caaa2e
Quentin Ducasse <quentin.ducasse@centralesupelec.fr>
2025-02-05 11:49:26 +0100

    added default gitignore for macos

--- install/git.sh
+++ install/git.sh
@@ -15,4 +15,9 @@ git config --global color.diff-highlight.newHighlight "green bold 22"
 # Create a nice last-change git log message, from https://twitter.com/elijahmanor/status/697055097356943360
 git config --global alias.lastchange 'log -p --follow -n 1'
 
+
+# Default gitignore for macos
+git-ignore macos > ${HOME}/.gitignore
+git config --global core.excludesFile '~/.gitignore'
+
 e_success "Done!"
