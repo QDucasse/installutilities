84578a3e44f921b8081ed9493c7ab3142c15be1d
Quentin Ducasse <quentin.ducasse@centralesupelec.fr>
2025-02-05 14:49:23 +0100

    git aliases

--- install/git.sh
+++ install/git.sh
@@ -12,12 +12,17 @@ git config --global color.diff-highlight.oldHighlight "red bold 52"
 git config --global color.diff-highlight.newNormal "green bold"
 git config --global color.diff-highlight.newHighlight "green bold 22"
 
-# Create a nice last-change git log message, from https://twitter.com/elijahmanor/status/697055097356943360
-git config --global alias.lastchange 'log -p --follow -n 1'
+# Aliases
 
+# - Show aliases
+git config set alias.aliases "config get --all --show-names --regexp '^alias\.'"
+# - Diff staged
+git config --global alias.diffs "diff --staged"
+# - Create a nice last-change git log message, from https://twitter.com/elijahmanor/status/697055097356943360
+git config --global alias.lastchange 'log -p --follow -n 1'
 
 # Default gitignore for macos
 git-ignore macos > ${HOME}/.gitignore
-git config --global core.excludesFile '~/.gitignore'
+git config --global core.excludesFile '${HOME}/.gitignore'
 
 e_success "Done!"
